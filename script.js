469744b4559fcdb032f429c399bf3db9c6f2af68
KhaledDeek <167533536+KhaledDeek@users.noreply.github.com>
2024-05-12 00:17:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-element1 = document.querySelectorAll(".num1")
-function changeD(){
-    for(i=0 ; i<element1.length; i++){
-        x = element1[i].innerText
-        b = x*9/5 + 32
-        element1[i].innerText = b
-    }
+function alertx() {
+    alert("loading weather repost ....")
 }
 
-element0 = document.querySelector(".cookies")
-function remove(){
+element1 = document.querySelectorAll(".num1")
+ele = document.getElementById("degree")
+
+function changeD() {
+    if (ele.value =="C"){
+        for(i=0 ; i<element1.length; i++){
+            x = element1[i].innerText 
+            b = (x-32)*5/9
+            element1[i].innerText =Math.floor(b) 
+        }
+    }
+    else if(ele.value =="F"){
+        for(i=0 ; i<element1.length; i++){
+            x = element1[i].innerText 
+            b = (x*9/5)+32
+            element1[i].innerText = Math.floor(b) 
+        }
+    }
+
+}
+
+
+function remove() {
+    element0 = document.querySelector(".cookies")
     element0.remove();
 }
